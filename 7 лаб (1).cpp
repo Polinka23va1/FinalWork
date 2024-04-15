53b652bdbf9acf8a0451db8599fdd6ac9d8d5ad8
Polinka23va1 <165585088+Polinka23va1@users.noreply.github.com>
2024-04-15 12:46:36 +0300

    Add files via upload

---
+++ 7 лаб (1).cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <fstream>
+#include <string>
+
+
+
+
+int const row = 5;
+int const col = 4;
+int arr[row][col];
+int ask;
+int count = 0;
+int sum[row];
+int nmin, bsum, barr;
+void input()
+	{
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				printf("Введите элемент массива %d строки, %d cтолбца: ", i + 1, j + 1);
+				scanf_s("%d\n", &arr[i][j]);
+			}
+		}
+	}
+void file_input()
+{
+	ifstream fin;
+	fin.open("mass.txt");
+	if (fin.is_open())
+	{
+		for (int i = 0; i < row; i++)
+		{
+			for (int j = 0; j < col; j++)
+			{
+				arr[i][j] = fin;
+			}
+		}
+	}
+	else
+	{
+		printf("Ошибка открытия файла");
+
+	}
+}
+
+
+int first_task()
+{
+	for (int i = 0; i < col; i++)
+	{
+		for (int j = 0; j < row; j++)
+		{
+			if (arr[j][i] == 0)
+			{
+				count += 1;
+				break;
+
+			}
+
+		}
+	}
+	return(col - count);
+}
+int second_task()
+{
+	for (int i = 0; i < row; i++)
+	{
+		sum[i] = 0;
+	}
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			if (arr[i][j] > 0 && arr[i][j] % 2 == 0)
+			{
+				sum[i] += arr[i][j];
+			}
+
+
+		}
+	}
+
+	
+	for (int i = 0; i < row - 1; i++)
+	{
+
+		for (int j = i + 1; j < row; j++)
+		{
+			nmin = i;
+			if (sum[j] < sum[nmin])
+				nmin = j;
+			bsum = sum[i];
+			sum[i] = sum[nmin];
+
+			sum[nmin] = bsum;
+			for (int j = 0; j < col; j++)
+			{
+				barr = arr[i][j];
+				arr[i][j] = arr[nmin][j];
+				arr[nmin][j] = barr;
+			}
+
+		}
+	}
+}
+void output()
+{
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			printf("%d ", arr[i][j]);
+			if (j == (col - 1))
+			{
+				printf("| %d", sum[i]);
+			}
+		}
+		printf("\n");
+	}
+}
+
+int main()
+	{
+		SetConsoleOutputCP(1251);
+		printf("Введите способ ввода данных:\n 1 - Ручной\n 2 - из файла\n");
+		scanf_s("%d", &ask);
+		if (ask == 1)
+		{
+			input();
+		}
+		if (ask == 2)
+		{
+			file_input();
+		}
+
+}
---
+++ 9 лаба (1).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h>
+int F(const int* arr, int n)
+{
+	if (n == 1) return arr[0];
+	return arr[n - 1] * F(arr, n - 1);
+}
+
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	int const N = 3;
+	int product = 1;
+	int arr[N];
+	for (int i = 0; i < N; i++)
+	{
+		printf("Введите %d-й элемент массива:\n", i+1);
+		scanf_s("%d", &arr[i]);
+	}
+	printf("Сумма элементов массива: %d", F(arr, N));
+	printf("Хуй");
+}
